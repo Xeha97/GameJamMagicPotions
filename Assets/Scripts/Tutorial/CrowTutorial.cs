5fefb6d7700a7d220df72163cb88c7f3c9d3e9b1
Xeha97 <jason.schlueter97@gmail.com>
2023-06-02 23:38:32 +0200

    Crow

---
+++ Assets/Scripts/Tutorial/CrowTutorial.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CrowTutorial : MonoBehaviour
+{
+    [SerializeField] private GameObject tutText;
+    [SerializeField] private Collider2D tutEnd;
+    
+    
+    
+    
+    
+}
